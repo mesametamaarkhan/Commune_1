1046df1b10f20fc447dc8d50bd2dae45668ea12a
mesametamaarkhan <i221304@nu.edu.pk>
2024-12-18 21:54:17 +0500

    frontend commit

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import cors from 'cors';
+import { PORT } from './config.js';
 
 const app = express();
 
@@ -7,6 +9,10 @@ app.use(express.json());
 
 //handling all cors (alternative is to use custom origins)
 app.use(cors()); 
+
+app.listen(PORT, () => {
+    console.log(`App is listening to port: ${PORT}`);
+});
 
 //landing page
 app.get('/', (req, res) => {
